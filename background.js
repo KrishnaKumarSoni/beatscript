b8d7f2fc6f356c058b4d6b243b325772b2f90a75
Krishna Kumar Soni <39898943+KrishnaKumarSoni@users.noreply.github.com>
2025-03-06 00:37:10 +0530

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.action.onClicked.addListener(async (tab) => {
-  try {
-    await chrome.tabs.sendMessage(tab.id, { action: 'toggleDrawer' });
-  } catch (error) {
-    console.log('Error sending message to content script:', error);
-  }
-}); 
